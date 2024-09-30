f6478e24f478c4bcb518abf5cbc0a949c7039da1
JulioAlb453 <fuliobeto18@gmail.com>
2024-09-30 06:25:01 -0500

    add: Routing, home, adoptame
    Se añade el enrutamiento,vista home y
    vista adoptame

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AdopcionComponent } from './adopcion/adopcion.component';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'adopcion',
+    component: AdopcionComponent
+  }
+];
