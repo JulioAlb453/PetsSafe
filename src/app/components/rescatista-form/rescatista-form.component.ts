4e9ee738b7e6fdd2a17d250dd9e0608a1f862fe4
JulioAlb453 <fuliobeto18@gmail.com>
2024-10-03 03:20:25 -0500

    add: rescatista form
    se crearon componente para la vista de rescatista-form asi tambien se modifico el css de los componentes de home

--- src/app/components/rescatista-form/rescatista-form.component.ts
+++ src/app/components/rescatista-form/rescatista-form.component.ts
@@ -1,12 +1,36 @@
-import { Component , Input} from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-rescatista-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './rescatista-form.component.html',
-  styleUrl: './rescatista-form.component.css'
+  styleUrls: ['./rescatista-form.component.css'] // Aquí corregí 'styleUrl' por 'styleUrls'
 })
 export class RescatistaFormComponent {
   @Input() rescatista: any;
+  rescatistaForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.rescatistaForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellidoPaterno: ['', Validators.required],
+      apellidoMaterno: ['', Validators.required],
+      telefono: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      edad: ['', Validators.required],
+      localizacion: ['', Validators.required],
+      tipoRescatista: ['', Validators.required],
+    });
+  }
+
+  actualizarDatos() {
+    if (this.rescatistaForm.valid) {
+      console.log('Formulario válido', this.rescatistaForm.value);
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
--- src/app/vistas/perfil-rescatista/perfil-rescatista.component.ts
+++ src/app/vistas/perfil-rescatista/perfil-rescatista.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RescatistaFormComponent } from '../../components/rescatista-form/rescatista-form.component';
 @Component({
   selector: 'app-perfil-rescatista',
   standalone: true,
-  imports: [],
+  imports: [RescatistaFormComponent],
   templateUrl: './perfil-rescatista.component.html',
   styleUrl: './perfil-rescatista.component.css'
 })
