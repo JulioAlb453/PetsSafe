06dc91a2cb21cb9447631f024eaeee783d09a2fd
JulioAlb453 <fuliobeto18@gmail.com>
2024-10-28 04:41:11 -0500

    fix app-component
    se arrego el app-component ya que se habia cambiado a modulo pero se reemplazo a standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], 
 })
